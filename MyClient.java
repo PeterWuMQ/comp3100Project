fa1e6cf6027f9575c7b4406b692aeb1161caf1b4
PeterWuMQ <peter.wu@students.mq.edu.au>
2022-03-30 17:46:18 +1100

    change file structure so Server class in is MyClient

--- MyClient.java
+++ MyClient.java
@@ -1,9 +1,9 @@
-package assignment;
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
-import assignment.Server;
+
 
 public class MyClient {
     public static void main(String[] args) {
@@ -170,4 +170,42 @@ public class MyClient {
         data = din.readLine();
         return data;
     }
+
+    static class Server {
+        private String type;
+        private int id;
+        private int cores;
+    
+        public Server(String type, int id, int cores) {
+            setType(type);
+            setId(id);
+            setCores(cores);
+        }
+    
+        public void setType(String type) {
+            this.type = type;
+        }
+    
+        public void setId(int id) {
+            this.id = id;
+        }
+    
+        public void setCores(int cores) {
+            this.cores = cores;
+        }
+    
+        public String getType() {
+            return type;
+        }
+    
+        public int getId() {
+            return id;
+        }
+    
+        public int getCores() {
+            return cores;
+        }
+    }
 }
+
+
