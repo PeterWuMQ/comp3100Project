171ba5db52740814545a3c8dd8e72b98267674cd
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2022-04-02 09:27:13 +1100

    Deleted ReadLine function and added comments

--- MyClient.java
+++ MyClient.java
@@ -4,23 +4,20 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MyClient {
     public static void main(String[] args) {
         Socket s = null;
         try {
             int serverPort = 50000;
             s = new Socket("127.0.0.1", serverPort);
-            
+
             BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            
+
             ProtocolHandshake(din, dout);
 
-            // ALT(din, dout, servers);
-
             LRR(din, dout);
-        
+
             WriteData(dout, "QUIT");
         } catch (UnknownHostException e) {
             System.out.println("Sock:" + e.getMessage());
@@ -38,174 +35,190 @@ public class MyClient {
         }
     }
 
+    // Performs initial handshake with server to establish connection
     private static void ProtocolHandshake(BufferedReader din, DataOutputStream dout) throws IOException {
         String data = "";
         String username = System.getProperty("user.name");
 
         WriteData(dout, "HELO");
 
-        data = ReadData(din);
+        data = din.readLine();
 
-        if (data.equals("OK")) {
-            WriteData(dout, "AUTH " + username);
-        }
+        WriteData(dout, "AUTH " + username);
 
-        data = ReadData(din);
+        data = din.readLine();
     }
 
+    // Sends request to server for all server information and returns a List of all
+    // servers
     private static List<Server> GetServers(BufferedReader din, DataOutputStream dout) throws IOException {
-            String data = "";
-
-            WriteData(dout, "GETS All");
-
-            data = ReadData(din);
-            String[] serverData = data.split(" ");
-            int serverNo = Integer.parseInt(serverData[1]);
-            List<Server> servers = new ArrayList<>();
-
-            WriteData(dout, "OK");
-
-            for(int i = 0; i < serverNo; i++) {
-                data = ReadData(din);
-                String[] splitServerData = data.split(" ");
-                servers.add(new Server(splitServerData[0], Integer.valueOf(splitServerData[1]), Integer.valueOf(splitServerData[4])));
-            }
-
-            WriteData(dout, "OK");
-            data = ReadData(din);
-
-            return servers;
-    }
-
-    private static void ALT(BufferedReader din, DataOutputStream dout) throws IOException {
-        int largest = 0;
-        Server largestServer = new Server("null", 0, 0);
         String data = "";
 
-        while(true) {
+        WriteData(dout, "GETS All");
+
+        // Get the amount of incoming server messages and initialise a empty list of
+        // servers
+        data = din.readLine();
+        String[] serverData = data.split(" ");
+        int serverNo = Integer.parseInt(serverData[1]);
+        List<Server> servers = new ArrayList<>();
+
+        WriteData(dout, "OK");
+
+        // parse every message and store as a List of servers
+        for (int i = 0; i < serverNo; i++) {
+            data = din.readLine();
+            String[] serverMessage = data.split(" ");
+            servers.add(new Server(serverMessage[0], Integer.valueOf(serverMessage[1]),
+                    Integer.valueOf(serverMessage[4])));
+        }
+
+        WriteData(dout, "OK");
+        data = din.readLine();
+
+        return servers;
+    }
+
+    // All To Largest - Schedules all jobs to the "largest" server (most amount of
+    // cores)
+    private static void ALT(BufferedReader din, DataOutputStream dout) throws IOException {
+        String data = "";
+        int largest = 0;
+        Server largestServer = new Server("null", 0, 0);
+
+        while (true) {
             WriteData(dout, "REDY");
 
-            data = ReadData(din);
+            // Get the next message
+            data = din.readLine();
+            String[] message = data.split(" ");
 
-            String[] jobSplit = data.split(" ");
-
-            if(largestServer.getType() == "null") {
+            // If largest server hasn't been found yet then get all servers and find the
+            // largest
+            if (largestServer.getType() == "null") {
                 List<Server> servers = GetServers(din, dout);
-                for(int i = 0; i < servers.size(); i++) {
+                for (int i = 0; i < servers.size(); i++) {
                     int temp = servers.get(i).getCores();
-                    if(largest < temp) {
+                    if (largest < temp) {
                         largest = temp;
                         largestServer = servers.get(i);
                     }
                 }
             }
 
-            if(jobSplit[0].equals("JOBN")) {
-                String schd = "SCHD " + jobSplit[2] + " " + largestServer.getType() + " " + Integer.valueOf(largestServer.getId());
+            // If message is a job submission then schedule it to the largest server
+            // If message is a job completion then do nothing
+            // If message is no more jobs then end
+            if (message[0].equals("JOBN")) {
+                String schd = "SCHD " + message[2] + " " + largestServer.getType() + " "
+                        + Integer.valueOf(largestServer.getId());
                 WriteData(dout, schd);
-                data = ReadData(din);
-            } else if(jobSplit[0].equals("JCPL")){
-            } else {
+                data = din.readLine();
+            } else if (message[0].equals("JCPL")) {
+            } else if (message[0].equals("NONE")) {
                 break;
             }
         }
     }
 
+    // Largest Round Robin - Schedules all jobs to servers of the same type as the
+    // server with the "largest" server (most amount of cores) in a round robin
+    // fashion
     private static void LRR(BufferedReader din, DataOutputStream dout) throws IOException {
         String data = "";
-
+        int current = 0;
         List<Server> largestServers = new ArrayList<>();
 
-        int current = 0;
-
-        while(true) {
+        while (true) {
             WriteData(dout, "REDY");
 
-            data = ReadData(din);
+            // Get the current
+            data = din.readLine();
+            String[] message = data.split(" ");
 
-            String[] jobSplit = data.split(" ");
-
-            if(largestServers.size() == 0) {
+            // If largest servers hasn't been found yet then get all servers
+            if (largestServers.isEmpty()) {
                 int largest = 0;
                 String largestType = " ";
                 List<Server> servers = GetServers(din, dout);
 
-                for(int i = 0; i < servers.size(); i++) {
+                // Find the largest type of server
+                for (int i = 0; i < servers.size(); i++) {
                     int temp = servers.get(i).getCores();
-                    if(largest < temp) {
+                    if (largest < temp) {
                         largest = temp;
                         largestType = servers.get(i).getType();
                     }
                 }
 
-                for(int i = 0; i < servers.size(); i++) {
-                    if(servers.get(i).getType().equals(largestType)) {
+                // Create a List of all server of the largest type
+                for (int i = 0; i < servers.size(); i++) {
+                    if (servers.get(i).getType().equals(largestType)) {
                         largestServers.add(servers.get(i));
                     }
                 }
             }
 
-            if(jobSplit[0].equals("JOBN")) {
-                String schd = "SCHD " + jobSplit[2] + " " + largestServers.get(current).getType() + " " + Integer.valueOf(largestServers.get(current).getId());
+            // If message is a job submission then schedule it to a server of the largest
+            // type and then increment current so the next job is scheduled to the next
+            // server of that type
+            // If message is a job completion then do nothing
+            // If message is no jobs then end
+            if (message[0].equals("JOBN")) {
+                String schd = "SCHD " + message[2] + " " + largestServers.get(current).getType() + " "
+                        + Integer.valueOf(largestServers.get(current).getId());
                 WriteData(dout, schd);
-                data = ReadData(din);
-                current ++;
-                if(current >= largestServers.size()) {
+                data = din.readLine();
+                current++;
+                if (current >= largestServers.size()) {
                     current = 0;
                 }
-            } else if(jobSplit[0].equals("JCPL")){
-            } else {
+            } else if (message[0].equals("JCPL")) {
+            } else if (message[0].equals("NONE")) {
                 break;
             }
         }
     }
 
+    // Writes data to server appending newline at the end
     private static void WriteData(DataOutputStream dout, String data) throws IOException {
         dout.write((data + "\n").getBytes());
         dout.flush();
-    }
-
-    private static String ReadData(BufferedReader din) throws IOException {
-        String data = "";
-        data = din.readLine();
-        return data;
     }
 
     static class Server {
         private String type;
         private int id;
         private int cores;
-    
+
         public Server(String type, int id, int cores) {
             setType(type);
             setId(id);
             setCores(cores);
         }
-    
+
         public void setType(String type) {
             this.type = type;
         }
-    
+
         public void setId(int id) {
             this.id = id;
         }
-    
+
         public void setCores(int cores) {
             this.cores = cores;
         }
-    
+
         public String getType() {
             return type;
         }
-    
+
         public int getId() {
             return id;
         }
-    
+
         public int getCores() {
             return cores;
         }
     }
 }
-
-
