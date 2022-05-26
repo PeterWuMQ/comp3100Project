7649c5bc428b78a20cf71fbc9d020f663fd84358
PeterWuMQ <peter.wu@students.mq.edu.au>
2022-05-26 17:01:08 +1000

    Implemented if no server is avaliable for bf then sche for unavaliable bf which aims to find the server with the least jobs

--- stage2/MyClient.java
+++ stage2/MyClient.java
@@ -8,8 +8,8 @@ import stage2.Server.ServerSortingComparator;
 
 public class MyClient {
     public static void main(String[] args) {
-        if (args.length != 2 || !args[0].equals("-a")) {
-            System.out.println("Usage: java MyClient -a");
+        if (args.length != 2 || !args[0].equals("-a") || !args[1].equals("sbf")) {
+            System.out.println("Usage: java MyClient -a sbf");
             System.exit(0);
         }
 
@@ -60,12 +60,13 @@ public class MyClient {
     // Sends request to server for all server information and returns a List of all
     // servers
     private static List<Server> getServers(BufferedReader din, DataOutputStream dout) throws IOException {
+        String data = "";
         writeData(dout, "GETS All");
 
         // Get the amount of incoming server messages and initialise a empty list of
         // servers
-        String[] serverData = readData(din).split(" ");
-        System.out.println(Arrays.toString(serverData));
+        data =  readData(din);
+        String[] serverData = data.split(" ");
         int serverNo = Integer.parseInt(serverData[1]);
         List<Server> servers = new ArrayList<>();
 
@@ -73,45 +74,44 @@ public class MyClient {
 
         // parse every message and store as a List of servers
         for (int i = 0; i < serverNo; i++) {
-            String[] serverMessage = readData(din).split(" ");
+            data =  readData(din);
+            String[] serverMessage = data.split(" ");
 
             String sType = serverMessage[0];
             String sId = serverMessage[1];
             int sCores = Integer.valueOf(serverMessage[4]);
             int sMem = Integer.valueOf(serverMessage[5]);
             int sDisk = Integer.valueOf(serverMessage[6]);
-            System.out.println(Arrays.toString(serverMessage));
 
             servers.add(new Server(sType, sId, sCores, sMem, sDisk));
         }
 
         Collections.sort(servers, new ServerSortingComparator());
 
-        for(int i = 0; i < servers.size(); i++) {
-            System.out.println(servers.get(i).getCores() + " " + servers.get(i).getMemory() + " " +  servers.get(i).getDisk());
-        }
-
         writeData(dout, "OK");
-        readData(din);
+        data =  readData(din);
 
         return servers;
     }
 
     private static void sbf(BufferedReader din, DataOutputStream dout) throws IOException {
+        String data = "";
         List<Server> servers = new ArrayList<>();
-        Server best = new Server("type", "id", 0, 0, 0);
-
+        
         while (true) {
+            Server best = new Server("null", "id", 0, 0, 0);
+
             writeData(dout, "REDY");
 
-            String[] message = readData(din).split(" ");
+            data = readData(din);
+            String[] message = data.split(" ");
 
             if (message[0].equals("JOBN")) {
                 
                 if(servers.size() == 0) {
                     servers = getServers(din, dout);
                     writeData(dout, "OK");
-                    readData(din);
+                    data = readData(din);
                 }
 
                 String jId = message[2];
@@ -123,19 +123,36 @@ public class MyClient {
                     int sCores = servers.get(i).getAvailCores();
                     int sMem = servers.get(i).getMemory();
                     int sDisk = servers.get(i).getDisk();
-
+                    
                     if(jCores <= sCores && jMem <= sMem && jDisk <= sDisk) {
                         best = servers.get(i);
                         servers.get(i).addJob(new Job(jId, jCores));
-                        System.out.println("job cores " + jCores);
-                        System.out.println("subtracted " + servers.get(i).getType() + " " + servers.get(i).getId() + " " + servers.get(i).getAvailCores());
                         break;
                     } 
                 }
 
-                scheduleJob(dout, message[2], best.getType(), best.getId());
+                int tempServer = servers.size() - 1;
+                int check = servers.get(tempServer).getAvailCores();
 
-                readData(din);
+                if(best.getId().equals("id")) {
+                    for(int i = 0; i < servers.size(); i++) {     
+                        int sCores = servers.get(i).getCores();
+                        int sAvaCores = servers.get(i).getAvailCores();
+                        int sMem = servers.get(i).getMemory();
+                        int sDisk = servers.get(i).getDisk();
+                        
+                        if(jCores <= sCores && jMem <= sMem && jDisk <= sDisk && check < sAvaCores) {
+                            check = sAvaCores;
+                            tempServer = i; 
+                        } 
+                    }
+                    best = servers.get(tempServer);
+                    servers.get(tempServer).addJob(new Job(jId, jCores));
+                }
+
+                scheduleJob(dout, jId, best.getType(), best.getId());
+
+                data = readData(din);
 
             } else if (message[0].equals("JCPL")) {
                 String cType = message[3];
@@ -145,10 +162,9 @@ public class MyClient {
                 for(int i = 0; i < servers.size(); i++) {
                     String sType = servers.get(i).getType();
                     String sId = servers.get(i).getId();
-                    
+
                     if(sId.equals(cId) && sType.equals(cType)) {
                         servers.get(i).removeJob(jId);
-                        System.out.println("added " + sType + " " + sId + " " + servers.get(i).getAvailCores());
                         break;
                     }
                 }
@@ -156,6 +172,12 @@ public class MyClient {
                 break;
             }
         }
+    }
+
+    public static void migrateJob(DataOutputStream dout, String jobID, String sType, String sID, String tType, String tID)
+            throws IOException {
+        String migj = "MIGJ " + jobID + " " + sType + " " + sID + " " + tType + " " + tID;
+        writeData(dout, migj);
     }
 
     public static void scheduleJob(DataOutputStream dout, String jobID, String serverType, String serverID)
@@ -166,11 +188,14 @@ public class MyClient {
 
     // Writes data to server appending newline at the end
     private static void writeData(DataOutputStream dout, String data) throws IOException {
+        System.out.println("S: " + data);
         dout.write((data + "\n").getBytes());
         dout.flush();
     }
 
     private static String readData(BufferedReader din) throws IOException {
-        return din.readLine();
+        String data = din.readLine();
+        System.out.println("R: " + data);
+        return data;
     }
 }
